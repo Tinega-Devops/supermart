6b40e457473a13149d8a075cce2d4b77fe8a847d
Tinega-Devops <tinega.onchari@simplealliance.com>
2021-08-04 15:18:12 +0300

    Added .env

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAMpB1J1dZgOFxfppB-BzMWKSyEEOqD-jc",
-    authDomain: "ecommerce-6c625.firebaseapp.com",
-    projectId: "ecommerce-6c625",
-    storageBucket: "ecommerce-6c625.appspot.com",
-    messagingSenderId: "44142611747",
-    appId: "1:44142611747:web:6ce801c72236b9fbdcd096"
+    apiKey: process.env.REACT_APP_apikey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
